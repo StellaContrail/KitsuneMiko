29ca9429c2c2f175f0db0ef213bd01a27b811c5b
rhango <a.i.rhango@gmail.com>
2017-09-27 18:05:39 +0900

    [追加] ボス用のActionをローテーションするConditionを追加
    * Actionをローテーションで実行するRotationConditionを追加
    * numでローテーション数を指定して，Checkの引数でローテーション番号
      を指定する

---
+++ Assets/Scripts/Conditions/Enemy/RotationCondition.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationCondition : Condition {
+    public int num;
+    int count = 0;
+
+    public override ConditionState Check (string[] args) {
+        ConditionState state = new ConditionState();
+        int order = int.Parse(args[0]);
+        if (order == count) {
+            state.isSatisfied = true;
+            count = (count + 1) % num;
+        }
+        return state;
+    }
+}
