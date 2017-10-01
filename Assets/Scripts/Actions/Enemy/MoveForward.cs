64b3a6da22784be194834e63c572dab56ff9a0f6
rhango <a.i.rhango@gmail.com>
2017-10-02 02:17:39 +0900

    [追加] Enemyの移動と方向転換を実装
    * MoveForward:
      + 前方に歩き続けるだけのAction
      + localScale.xによって方向を判断している
    * TurnDirection:
      + 方向転換するAction
      + localScale.xの符号を反転させている
    * BlockExistFront:
      + 目の前にlayerがBlockのオブジェクトがあるか判定する
      + Blockがあるときにtrueとなる
      + 前方の判断はlocalScale.xによって行っている

---
+++ Assets/Scripts/Actions/Enemy/MoveForward.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveForward : Action {
+    public float speed;
+
+    Rigidbody2D rbody;
+
+    void Start () {
+        rbody = GetComponent<Rigidbody2D>();
+    }
+
+    public override bool IsDone () {
+        return true;
+    }
+
+    public override void Act (Dictionary<string, object> args) {
+        rbody.velocity = new Vector2(transform.localScale.x * speed, rbody.velocity.y);
+    }
+}
---
+++ Assets/Scripts/Actions/Enemy/TurnDirection.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnDirection : Action {
+    public override bool IsDone () {
+        return true;
+    }
+
+    public override void Act (Dictionary<string, object> args) {
+        transform.localScale
+            = new Vector2(-1 * transform.localScale.x, transform.localScale.y);
+    }
+}
