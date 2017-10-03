a11a4fc90233a2b4fd1913870e4ed311383bb606
rhango <a.i.rhango@gmail.com>
2017-10-04 00:10:23 +0900

    [追加] Enemyの近接攻撃のActionを追加
    * 子オブジェクトがあたり判定を管理する
    * 子オブジェクトにはDamage, Rigidbody2D, collider2Dを付ける必要がある

---
+++ Assets/Scripts/Actions/Enemy/MeleeAttack.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeAttack : Action {
+
+    GameObject weapon;
+
+    void Start () {
+        weapon = GetComponentInChildren<Damage>().gameObject;
+    }
+
+    bool _isDone = true;
+    public override bool IsDone () {
+        return _isDone;
+    }
+
+    public void OnAnimationEnd () {
+        weapon.SetActive(false);
+        _isDone = true;
+    }
+
+    public override void Act (Dictionary<string, object> args) {
+        if (!_isDone) {
+            return;
+        }
+        _isDone = false;
+        weapon.SetActive(true);
+    }
+}
