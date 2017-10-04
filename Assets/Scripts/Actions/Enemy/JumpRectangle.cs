381c0efbc26d18f451c1e64ff3c1dc2a11940013
DESKTOP-980CBK9\flagf <fragfighter.mswad@gmail.com>
2017-10-04 18:26:44 +0900

    [変更] JumpRectangle.csで一面ボスの矩形移動を実装
    一面ボスの矩形移動を実装,
    矩形移動のデバッグ用オブジェクト「Boss」を作成

--- Assets/Scripts/Actions/Enemy/JumpRectangle.cs
+++ Assets/Scripts/Actions/Enemy/JumpRectangle.cs
@@ -1,16 +1,142 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
+public class JumpRectangle : Action
+{
+    private const int ENEMY_POINT = 50;//敵の得点
+    private GameObject gameManager;//ゲームマネージャー
 
-public class JumpRectangle : MonoBehaviour {
+    public LayerMask blockLayer;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private Rigidbody2D rbody;
+
+    private float moveSpeed = 2;
+
+    public enum MOVE_DIR
+    {
+        LEFT,
+        RIGHT,
+        UP,
+        DOWN,
+    }
+
+    private MOVE_DIR moveDirection = MOVE_DIR.UP;//移動方法
+
+    // Use this for initialization
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody2D>();
+        gameManager = GameObject.Find("GameManager");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        bool isBlock = false;
+
+        Vector3 bossPosition;
+
+        if (moveDirection == MOVE_DIR.UP)
+        {
+            rbody.velocity = new Vector2(0, rbody.velocity.y + 0.5f);
+            //transform.localScale = new Vector2(1, 1);
+
+            bossPosition = transform.position;
+
+            if (bossPosition.y > 3.3f)
+            {
+                if (bossPosition.x < 0)
+                {
+                    moveDirection = MOVE_DIR.RIGHT;
+                    transform.localScale = new Vector2(-1, 1);
+                }
+                else
+                {
+                    moveDirection = MOVE_DIR.LEFT;
+                    transform.localScale = new Vector2(1, 1);
+
+                }
+            }
+        }
+
+        if (moveDirection == MOVE_DIR.RIGHT)
+        {
+            rbody.velocity = new Vector2(moveSpeed, 0);
+            //transform.localScale = new Vector2(1, 1);
+
+            bossPosition = transform.position;
+
+            if (bossPosition.x > 6.0f)
+            {
+                moveDirection = MOVE_DIR.DOWN;
+                transform.localScale = new Vector2(-1, 1);
+
+            }
+
+        }
+
+        if (moveDirection == MOVE_DIR.LEFT)
+        {
+            rbody.velocity = new Vector2(-moveSpeed, 0);
+            //transform.localScale = new Vector2(1, 1);
+
+            bossPosition = transform.position;
+
+            if (bossPosition.x > -6.0f)
+            {
+                moveDirection = MOVE_DIR.DOWN;
+                transform.localScale = new Vector2(1, 1);
+
+            }
+
+        }
+
+        if (moveDirection == MOVE_DIR.DOWN)
+        {
+            rbody.velocity = new Vector2(0, rbody.velocity.y - 0.5f);
+            transform.localScale = new Vector2(1, 1);
+
+            bossPosition = transform.position;
+
+        }
+
+
+
+    }
+
+    public override bool IsDone()
+    {
+        return true;
+    }
+
+    public override void Act(Dictionary<string, object> args)
+    {
+
+    }
+
+    public void DestroyEnemy()
+    {
+        gameManager.GetComponent<GameManager>().AddScore(ENEMY_POINT);
+
+        rbody.velocity = new Vector2(0, 0);
+        //コライダー削除
+        CircleCollider2D circleCollider = GetComponent<CircleCollider2D>();
+        BoxCollider2D boxCollider = GetComponent<BoxCollider2D>();
+        Destroy(circleCollider);
+        Destroy(boxCollider);
+        //死亡モーション
+        Sequence animSet = DOTween.Sequence();
+        animSet.Append(transform.DOLocalMoveY(0.5f, 0.2f).SetRelative());
+        animSet.Append(transform.DOLocalMoveY(-10.0f, 1.0f).SetRelative());
+
+        Destroy(this.gameObject, 1.2f);
+    }
+
 }
