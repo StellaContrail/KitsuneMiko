a010f8fb65dbd062ea345e7d806c5fe8184dbd91
DESKTOP-0A53A5B\まほ <jyuraku.2013@gmail.com>
2017-10-09 20:19:08 +0900

    [追加]敵・狐の消滅エフェクト、竜巻エフェクト（大き目）などを追加しました

---
+++ Assets/Resources/Effect/AuraScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class AuraScript : MonoBehaviour {
+	//ゲームが始まった際に動画を再生させる
+	public MovieTexture movie;
+
+	void Start(){
+		movie.Play();
+
+	}
+}
