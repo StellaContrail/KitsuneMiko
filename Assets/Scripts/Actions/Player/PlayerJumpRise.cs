3ecafc8115cd0509fd6861620220459c3d34781a
Contrail Sky <Contrail Sky>
2017-10-10 21:47:58 +0900

    [追加] Player関連スクリプトのAssets追加
    * 攻撃スクリプトの追加
    * チャージスクリプトの追加
    * ジャンプスクリプトの追加
    * 歩行スクリプトの追加

---
+++ Assets/Scripts/Actions/Player/PlayerJumpRise.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJumpRise : Action
+{
+
+    Rigidbody2D rbody;
+    public float jumpForce = 200f;//ジャンプ力
+	[System.NonSerialized]
+    public bool isJumping = false;
+
+    bool _IsDone = true;
+    public override bool IsDone()
+    {
+        return _IsDone;
+    }
+
+    public override void Act(Dictionary<string, object> args)
+    {
+        rbody.AddForce(Vector2.up * jumpForce);
+        isJumping = true;
+        _IsDone = true;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        rbody = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Actions/Player/PlayerWalk.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWalk : Action {
+
+    bool _isDone = true;
+
+    public override bool IsDone()
+    {
+        return _isDone;
+    }
+
+    public float walkSpeed = 3f;
+    public override void Act(Dictionary<string, object> args)
+    {
+        float moveSpeed = 0f;
+        Rigidbody2D rbody = gameObject.GetComponent<Rigidbody2D>();
+        switch ((PlayerWalkCondition.WALK_DIR)System.Enum.Parse(typeof(PlayerWalkCondition.WALK_DIR), (string)args["movingDirection"]))
+        {
+            case PlayerWalkCondition.WALK_DIR.IDLE:
+                _isDone = true;
+                moveSpeed = 0;
+                break;
+            case PlayerWalkCondition.WALK_DIR.LEFT:
+                _isDone = false;
+                moveSpeed = walkSpeed * -1;
+                transform.localScale = new Vector2(1, 1);
+                break;
+            case PlayerWalkCondition.WALK_DIR.RIGHT:
+                _isDone = false;
+                moveSpeed = walkSpeed;
+                transform.localScale = new Vector2(-1, 1);
+                break;
+        }
+        rbody.velocity = new Vector2(moveSpeed, rbody.velocity.y);
+    }
+}
