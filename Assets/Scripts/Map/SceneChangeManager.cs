c116d3136ebe76e72d0f6ccc7817417a4b9cf1b0
mr_pugi <mrpugiedp@gmail.com>
2017-10-16 18:55:15 +0900

    摩擦の消去 シーン遷移

---
+++ Assets/Scripts/Map/SceneChangeManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChangeManager : MonoBehaviour {
+
+    public string StageName;
+    public GameObject player;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+
+        if (collision.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(StageName);
+            player.GetComponent<PlayerManager>().sceneChange();
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
