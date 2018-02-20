7aaaea301e70a5966a43aa9a8f3614ccdfcbf4fd
StellaContrail <sky.contrail.man+github@gmail.com>
2018-02-20 18:01:05 +0900

    [追加] スキル「かまいたち」の追加
    * MoonBulletにRigidbody2D、MoonBullet.csの追加
    * MoonBullet.cs、Kamaitachi.csの追加

---
+++ Assets/MoonBullet.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoonBullet : RangedWeapon {
+
+    static readonly float V_IX = 5.0f;
+    static readonly float V_IY = 10.0f;
+    static readonly float OMEGA = 0.0f;
+
+    public override void Init (Transform attacker) {
+        base.Init(attacker);
+        Rigidbody2D rbody = GetComponent<Rigidbody2D>();
+        float dir = (float)attacker.GetFaceDir();
+        rbody.velocity = new Vector2(dir * V_IX, V_IY);
+        rbody.angularVelocity = dir * OMEGA;
+    }
+
+    void OnTriggerEnter2D (Collider2D col) {
+        if (col.tag != tag) {
+            Destroy(gameObject);
+        }
+    }
+}
