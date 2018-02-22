59a6a0103db5fdeddfa4432cad7907213313b196
StellaContrail <sky.contrail.man+github@gmail.com>
2018-02-22 18:28:19 +0900

    [追加] Invinsible.csを追加
    * 無敵スキル用のスクリプト

---
+++ Assets/Scripts/Skills/Invinsible.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Invinsible : Skill {
+
+	public override float cost {
+        get {
+            return 0.5f;
+        }
+    }
+
+    protected override void Awake () {
+        breakable = GetComponent<Breakable>();
+        base.Awake();
+    }
+
+}
