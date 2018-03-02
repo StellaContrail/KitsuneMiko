d75149de62d78a050b5883a82952e05d49d5b69e
StellaContrail <sky.contrail.man+github@gmail.com>
2018-03-02 15:36:07 +0900

    [追加] Dash.csの追加
    * 空中、地上でダッシュを可能にするスキルのスクリプト

---
+++ Assets/Scripts/Skills/Dash.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dash : Skill {
+
+    int intervalTime;
+    KeyCode pressedKeyCode = KeyCode.None;
+
+    public override float cost
+    {
+        get
+        {
+            return 0.8f;
+        }
+    }
+
+	protected override void Awake()
+    {
+        base.Awake();
+    }
+
+	/// <summary>
+	/// Update is called every frame, if the MonoBehaviour is enabled.
+	/// </summary>
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.RightArrow))
+		{
+            pressedKeyCode = KeyCode.RightArrow;
+        }
+	}
+
+	/// <summary>
+	/// This function is called every fixed framerate frame, if the MonoBehaviour is enabled.
+	/// </summary>
+	void FixedUpdate()
+	{
+		
+	}
+
+}
