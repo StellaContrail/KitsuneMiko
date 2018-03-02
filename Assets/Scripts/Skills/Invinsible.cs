050f58aaf4253e6d7069252b6eade4ad26de5cd3
StellaContrail <sky.contrail.man+github@gmail.com>
2018-03-02 15:38:53 +0900

    [修正] 無敵スキルの時間を外した後もとに戻らなかった問題を修正
    * 元の設定されている無敵時間を変数に一時的に入れて、スキル無効化の時にそれを適応するようにした

--- Assets/Scripts/Skills/Invinsible.cs
+++ Assets/Scripts/Skills/Invinsible.cs
@@ -4,7 +4,13 @@ using UnityEngine;
 
 public class Invinsible : Skill {
 
-	public override float cost {
+    Breakable breakable;
+
+    public int DURATION = 50;
+    int _invFrameNum;
+
+
+    public override float cost {
         get {
             return 0.5f;
         }
@@ -13,6 +19,32 @@ public class Invinsible : Skill {
     protected override void Awake () {
         breakable = GetComponent<Breakable>();
         base.Awake();
+        _invFrameNum = breakable.invFrameNum;
+    }
+
+    /// <summary>
+    /// This function is called when the object becomes enabled and active.
+    /// </summary>
+    void OnEnable()
+    {
+        breakable.invFrameNum = DURATION;
+        breakable.BeginInvincible();
+    }
+
+    /// <summary>
+    /// This function is called every fixed framerate frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    void FixedUpdate()
+    {
+        
+    }
+
+    /// <summary>
+    /// This function is called when the behaviour becomes disabled or inactive.
+    /// </summary>
+    void OnDisable()
+    {
+        breakable.invFrameNum = _invFrameNum;
     }
 
 }
