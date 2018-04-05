0d9c53553d94688752b219554b1daee9b2dda298
Contrail <sky.contrail.man+github@gmail.com>
2018-04-06 01:57:13 +0900

    [追加] エフェクトが自己制御するためのスクリプトを追加
    * 今の所は自分のエフェクトが再生し終わったら自分をDestroyする機能のみ

---
+++ Assets/Scripts/EffectSelfController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectSelfController : MonoBehaviour {
+
+    public bool isDestinedToDie = false;
+	/// <summary>
+	/// Start is called on the frame when a script is enabled just before
+	/// any of the Update methods is called the first time.
+	/// </summary>
+	void Start()
+	{
+        Destroy(gameObject, gameObject.transform.GetChild(0).GetComponent<ParticleSystem>().main.duration);
+    }
+}
