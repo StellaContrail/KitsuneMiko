bf20942b022a80f85f6f5b3b0d547136aca9c14b
Contrail <sky.contrail.man+github@gmail.com>
2018-04-06 03:18:54 +0900

    [追加] エフェクト自己制御スクリプトに時間指定を追加
    * Now effect can destroy itself according to the time of "aliveTime" when it's not negative.

--- Assets/Scripts/EffectSelfController.cs
+++ Assets/Scripts/EffectSelfController.cs
@@ -5,12 +5,24 @@ using UnityEngine;
 public class EffectSelfController : MonoBehaviour {
 
     public bool isDestinedToDie = false;
-	/// <summary>
-	/// Start is called on the frame when a script is enabled just before
-	/// any of the Update methods is called the first time.
-	/// </summary>
-	void Start()
+    public float aliveTime = -1f;
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
+    /// </summary>
+    void Start()
 	{
-        Destroy(gameObject, gameObject.transform.GetChild(0).GetComponent<ParticleSystem>().main.duration);
+		if (isDestinedToDie)
+		{
+			if (aliveTime >= 0)
+			{
+				Destroy(gameObject, aliveTime);
+			}
+			else
+			{
+        		Destroy(gameObject, gameObject.transform.GetChild(0).GetComponent<ParticleSystem>().main.duration);
+			}
+			
+		}
     }
 }
